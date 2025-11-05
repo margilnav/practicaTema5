b97bccde98f302521488008ff566c250c00e8e39
margilnav <margilnav@alu.edu.gva.es>
2025-11-05 12:03:04 +0100

    Cambio 1

---
+++ java/src/main/java/org/example/CodigiPracticaAnterior.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+public class CodigiPracticaAnterior {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce tu año de nacimiento:");
+        int anyo = sc.nextInt();
+        LocalDateTime fechaActual = LocalDateTime.now();
+        int edad = fechaActual.getYear()+anyo;
+        if(edad>18){
+            System.out.println("Eres mayor de edad");
+        }
+        else{
+            System.out.println("Eres menor de edad");
+        }
+    }
+}
+
